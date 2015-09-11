56c2439be898f2adf71def86cf984780c7791b82
Code Whisperer <daniel@danielstern.ca>
2015-09-11 13:37:33 -0400

    Add content to server.js

--- server.js
+++ server.js
@@ -13,11 +13,26 @@ var items = [{
     description:"This stylish cardboard box will hide you from all sorts of tactical personnel",
     priceUSD:"34.95"
 },{
-    id:042089,
+    id:042030,
     name:"Camera",
     description:"What better way to share that beautiful Costa Rican vista with your friends?",
     priceUSD:"155.95"
+},{
+    id:042031,
+    name:"Magazine",
+    description:"One can't help but be distracted by this magazine.",
+    priceUSD:"9.00"
 }];
+
+var promotions = [{
+    applyTo:[042089],
+    reducePriceByUSD:5
+}];
+
+var conversions = {
+    USDtoCAD:1.5,
+    USDtoGBP:0.9
+};
 
 server.get('/',function(req,res){
     var app = React.createFactory(require('./app/main.jsx'));
