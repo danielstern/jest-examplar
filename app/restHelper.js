2a43f1ab801b2924437215bc855229a420542077
Code Whisperer <daniel@danielstern.ca>
2015-09-13 05:24:24 -0400

    Add simplified rest helper from React-Express

---
+++ app/restHelper.js
@@ -0,0 +1,14 @@
+let $ = require('jquery');
+
+module.exports = {
+	get(url){
+		return new Promise(function(success,error){
+			$.ajax({
+				url:url,
+				dataType:"json",
+				success,
+				error
+			});
+		});
+	}
+}
