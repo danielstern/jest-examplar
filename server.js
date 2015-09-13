693298c57da3ef239a3da59bc7f7614f7fae611e
Code Whisperer <daniel@danielstern.ca>
2015-09-13 05:47:29 -0400

    add rates route

--- server.js
+++ server.js
@@ -6,18 +6,21 @@ var server = new express();
 
 server.set("view engine","ejs");
 server.set("views","./");
+server.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 var items = require('./items.js');
+
+var conversions = {
+  USDtoCAD:1.5,
+  USDtoGBP:0.9
+
+};
 
 var promotions = [{
     applyTo:[042089],
     reducePriceByUSD:5
 }];
 
-var conversions = {
-    USDtoCAD:1.5,
-    USDtoGBP:0.9
-};
 
 server.get('/',function(req,res){
     var app = React.createFactory(require('./app/main.jsx'));
@@ -26,6 +29,9 @@ server.get('/',function(req,res){
         items
     }));
     res.render('app/index',{app:generated});
-});
+})
+.get("/rates",function(req,res){
+  res.json(conversions);
+})
 
 server.listen(80);
