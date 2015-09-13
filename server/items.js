78ed6ba359bc9daceb166ac410dd223c2be8e89a
Code Whisperer <daniel@danielstern.ca>
2015-09-13 08:00:11 -0400

    move backend files to server directory

--- server/items.js
+++ server/items.js
@@ -1,15 +1,15 @@
 module.exports = [{
-    id:"042089",
+    id:"401",
     name:"Box",
     description:"It's just an ordinary box, or is it?",
     priceUSD:34.95
 },{
-    id:"042030",
+    id:"402",
     name:"Camera",
     description:"This small, lightweight camera can hold over 1000 high definition images.",
     priceUSD:155.95
 },{
-    id:"042031",
+    id:"403",
     name:"Magazine",
     description:"One can't help but be distracted by this magazine.",
     priceUSD:9
--- server/server.js
+++ server/server.js
@@ -6,24 +6,26 @@ var server = new express();
 
 server.set("view engine","ejs");
 server.set("views","./");
-server.use('/bower_components',  express.static(__dirname + '/bower_components'));
-server.use('/',  express.static(__dirname + '/.tmp'));
+console.log(__dirname);
+server.use('/bower_components',  express.static(__dirname + '/../bower_components'));
+server.use('/',  express.static(__dirname + '/../.tmp'));
 
 var items = require('./items.js');
 
 var conversions = {
   USDtoCAD:1.5,
   USDtoGBP:0.9
-
 };
 
 var promotions = [{
-    applyTo:[042089],
-    reducePriceByUSD:5
+    id:[501],
+    applyTo:[401],
+    reducePriceByUSD:5,
+    name:"President's Day Sale"
 }];
 
 server.get('/',function(req,res){
-    var app = React.createFactory(require('./app/main.jsx'));
+    var app = React.createFactory(require('./../app/main.jsx'));
 
     var generated = React.renderToString(app({
         items,
