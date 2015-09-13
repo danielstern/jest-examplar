0a8428cf7585ba65125cfb619c671f35dd1f7853
Code Whisperer <daniel@danielstern.ca>
2015-09-12 20:43:47 -0400

    fix invalid number

--- items.js
+++ items.js
@@ -1,16 +1,16 @@
 module.exports = [{
-    id:042089,
+    id:"042089",
     name:"Box",
     description:"It's just an ordinary box, or is it?",
-    priceUSD:"34.95"
+    priceUSD:34.95
 },{
-    id:042030,
+    id:"042030",
     name:"Camera",
     description:"This small, lightweight camera can hold over 1000 high definition images.",
-    priceUSD:"155.95"
+    priceUSD:155.95
 },{
-    id:042031,
+    id:"042031",
     name:"Magazine",
     description:"One can't help but be distracted by this magazine.",
-    priceUSD:"9.00"
+    priceUSD:9
 }];
