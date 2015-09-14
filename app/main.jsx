ff64acc0389881eea210743e073bb2219581041b
Code Whisperer <daniel@danielstern.ca>
2015-09-14 16:55:31 -0400

    add render function (isomorph)

--- app/main.jsx
+++ app/main.jsx
@@ -3,11 +3,17 @@ let React = require("react/addons");
 let CartItem = require('./components/CartItem.jsx');
 let promotionsHelper = require('./helpers/promotionsHelper.js');
 
+let itemStore = require('./stores/ItemStore.js');
+
+let LocalizationBox = require('./components/LocalizationBox.js');
+
+
 let CartApp = React.createClass({
 
     render(){
         return (
             <div>
+                <LocalizationBox />
                 <h3>Your Cart ({this.props.items.length})</h3>
                 <div>
                 {this.props.items.map((item, index)=>{
@@ -18,5 +24,23 @@ let CartApp = React.createClass({
         )
     }
 });
+
+let items = [];
+itemStore.onChange(()=>{
+  items = itemStore.getItems();
+  render();
+});
+
+let render = ()=>{
+    if (typeof window !== 'undefined') {
+      React.render(<CartApp items={items}/>,document.getElementById('mount'));
+    }
+}
+
+
+
+
+
+
 
 module.exports = CartApp;
