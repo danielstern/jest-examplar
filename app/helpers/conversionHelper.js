0bf2c7b93b4a47117b9f935e1de9d5d50d166635
Code Whisperer <daniel@danielstern.ca>
2015-09-15 09:33:02 -0400

    improve conversion helper

--- app/helpers/conversionHelper.js
+++ app/helpers/conversionHelper.js
@@ -1,5 +1,18 @@
-module.exports = {
-  convertUSDtoCad(USD){
+var symbols = {
+  "USA":"$"
+}
 
+var rates = {
+  "USA":1,
+  "CAN":1.15,
+  "GB":0.65
+}
+
+module.exports = {
+  convertFromUSD(locale,amount){
+    return amount * rates[locale];
+  },
+  getSymbolFor(locale){
+    return symbols[locale];
   }
 }
