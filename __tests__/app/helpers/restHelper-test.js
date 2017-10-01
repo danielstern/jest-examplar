7bf2c906d707c1c4a0a9301af4e4c73d554499bd
Daniel Stern <daniel@danielstern.ca>
2017-10-01 07:15:28 -0400

    most test works

--- __tests__/app/helpers/restHelper-test.js
+++ __tests__/app/helpers/restHelper-test.js
@@ -2,24 +2,24 @@
 
 jest.dontMock('../../../app/helpers/restHelper.js');
 describe('the GET Helper', function() {
-	// it('makes a GET request with the correct paramaters', function() {
+	it('makes a GET request with the correct paramaters', function() {
     //
-	// 	let $ = require('jquery');
-	// 	let restHelper = require('../../../app/helpers/restHelper.js');
-	// 	let testURL = 'api/test';
-    //
-	// 	// Call into the function we want to test
-	// 	restHelper.get(testURL,x=>x);
-    //
-	// 	// Verify everything works correctly
-	// 	expect($.ajax).toBeCalledWith({
-	// 		type: 'GET',
-	// 		dataType:'json',
-	// 		url: testURL,
-	// 		success: jasmine.any(Function),
-	// 		error: jasmine.any(Function)
-	// 	});
-	// });
+		let $ = require('jquery');
+		let restHelper = require('../../../app/helpers/restHelper.js');
+		let testURL = 'api/test';
+
+		// Call into the function we want to test
+		restHelper.get(testURL,x=>x);
+
+		// Verify everything works correctly
+		expect($.ajax).toBeCalledWith({
+			type: 'GET',
+			dataType:'json',
+			url: testURL,
+			success: jasmine.any(Function),
+			error: jasmine.any(Function)
+		});
+	});
 	//
 	// it("sends over the correct values returned by jQuery",function(done){
 	//
