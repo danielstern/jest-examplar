492a0a2e18d281463eebc145d604032e602d1e2e
Daniel Stern <daniel@danielstern.ca>
2017-10-01 07:19:51 -0400

    All working!

--- __tests__/app/helpers/restHelper-test.js
+++ __tests__/app/helpers/restHelper-test.js
@@ -1,10 +1,14 @@
 "use strict";
 
 jest.dontMock('../../../app/helpers/restHelper.js');
+jest.setMock('jquery',{
+    ajax:jest.genMockFn()
+});
 describe('the GET Helper', function() {
 	it('makes a GET request with the correct paramaters', function() {
     //
 		let $ = require('jquery');
+		// $.ajax = jest.fn();
 		let restHelper = require('../../../app/helpers/restHelper.js');
 		let testURL = 'api/test';
 
@@ -21,36 +25,36 @@ describe('the GET Helper', function() {
 		});
 	});
 	//
-	// it("sends over the correct values returned by jQuery",function(done){
-	//
-	// 	let returnedValue = null;
-	// 	let value = {test:true};
-	//
-	// 	runs(function(){
-	// 		let promise;
-	// 		let $ = require('jquery');
-	//
-	// 		$.ajax.mockImplementation(function(obj){
-	// 			obj.success(value);
-	// 		});
-	//
-	//
-	// 		let restHelper = require.requireActual('../../../app/helpers/restHelper.js');
-	// 		let testURL = 'api/test';
-    //
-	// 		restHelper.get(testURL).then(function(a){
-	// 			returnedValue = a;
-	// 		});
-	//
-	// 		jest.runAllTimers();
-	// 	});
-	//
-	// 	waitsFor(function(){
-	// 		return returnedValue;
-	// 	});
-	//
-	// 	runs(function(){
-	// 		expect(returnedValue).toEqual(value);
-	// 	})
-	// })
+	it("sends over the correct values returned by jQuery",function(done){
+
+		let returnedValue = null;
+		let value = {test:true};
+
+		runs(function(){
+			let promise;
+			let $ = require('jquery');
+
+			$.ajax.mockImplementation(function(obj){
+				obj.success(value);
+			});
+
+
+			let restHelper = require.requireActual('../../../app/helpers/restHelper.js');
+			let testURL = 'api/test';
+
+			restHelper.get(testURL).then(function(a){
+				returnedValue = a;
+			});
+
+			jest.runAllTimers();
+		});
+
+		waitsFor(function(){
+			return returnedValue;
+		});
+
+		runs(function(){
+			expect(returnedValue).toEqual(value);
+		})
+	})
 });
